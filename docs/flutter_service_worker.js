15466341b8d00034d6f1487f9c2bcfc199d2bb97
Chris Norman CRCB <chris@crcb.org.uk>
2023-08-04 15:05:31 +0100

    Rebuilt web.

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,8 +26,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "a5342eeb9b1148172fd3318fbd01f607
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c6d1b6daeffc352a13547536596d5cb8",
-"/": "c6d1b6daeffc352a13547536596d5cb8",
+"index.html": "3c198b44140986368962b42796db4f40",
+"/": "3c198b44140986368962b42796db4f40",
 "main.dart.js": "de869927258d54878289017eb65cf17c",
 "manifest.json": "919d4899eec782662344d676ac13618b",
 "version.json": "60e5b914df9b0ef102ebb647c60ae760"};
