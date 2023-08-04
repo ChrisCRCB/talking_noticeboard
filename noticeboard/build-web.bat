d4f9e2efd39210b06011377d65f4dcebcb52b303
Chris Norman CRCB <chris@crcb.org.uk>
2023-08-04 15:06:02 +0100

    Add git commands to build script.

--- noticeboard/build-web.bat
+++ noticeboard/build-web.bat
@@ -1,2 +1,2 @@
 @echo off
-rm -rf ..\docs & flutter build web --release & mv build\web .. & rename ..\web docs
+rm -rf ..\docs & flutter build web --release & mv build\web .. & rename ..\web docs & git add ..\docs & git commit -m "Rebuilt web." & git push
