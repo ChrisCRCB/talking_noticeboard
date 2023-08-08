bd7c158892d89cf31d947650b783ba5a1a7e9e88
Chris Norman CRCB <chris@crcb.org.uk>
2023-08-08 14:19:39 +0100

    Supply a prefix for the building script.

--- noticeboard/build-web.bat
+++ noticeboard/build-web.bat
@@ -1,2 +1,2 @@
 @echo off
-rm -rf ..\docs & flutter build web --release & mv build\web .. & rename ..\web docs & git add ..\docs & git commit -m "Rebuilt web." & git push
+rm -rf ..\docs & flutter build web --release --base-href /talking_noticeboard/ & mv build\web .. & rename ..\web docs & git add ..\docs & git commit -m "Rebuilt web." & git push
