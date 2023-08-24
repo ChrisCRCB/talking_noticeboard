41ac50df76303e8a3200c5c589fb6125b2718118
Chris Norman CRCB <chris@crcb.org.uk>
2023-08-24 12:42:29 +0100

    Try to unlock the screen before opening the app.

--- noticeboard/run-adb.bat
+++ noticeboard/run-adb.bat
@@ -1,2 +1,2 @@
 @echo off
-adb shell am start -n uk.org.crcb.noticeboard/uk.org.crcb.noticeboard.MainActivity
+adb shell input keyevent KEYCODE_WAKEUP & adb shell am start -n uk.org.crcb.noticeboard/uk.org.crcb.noticeboard.MainActivity
