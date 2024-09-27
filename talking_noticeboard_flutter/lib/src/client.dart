dff288852cb45ca8830086c7815737ef331d2211
Chris Norman CRCB <chris@crcb.org.uk>
2024-09-27 15:00:44 +0100

    Require compile time constants for the client URL.

--- talking_noticeboard_flutter/lib/src/client.dart
+++ talking_noticeboard_flutter/lib/src/client.dart
@@ -2,9 +2,15 @@ import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 import 'package:talking_noticeboard_client/talking_noticeboard_client.dart';
 
+/// The protocol to use for [client].
+const clientProtocol = String.fromEnvironment('client_protocol');
+
+/// The hostname to use for the [client].
+const clientHostname = String.fromEnvironment('client_hostname');
+
 /// The client to use.
 final client = Client(
-  'http://backstreets.site:6080/',
+  '$clientProtocol://$clientHostname:6080/',
   authenticationKeyManager: FlutterAuthenticationKeyManager(),
 )..connectivityMonitor = FlutterConnectivityMonitor();
 
